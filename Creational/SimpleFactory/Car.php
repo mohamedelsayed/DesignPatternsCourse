1e53f441dac95e656d11d442ffd83a4b6d330d4d
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-08 02:49:24 +0200

    SimpleFactory

---
+++ Creational/SimpleFactory/Car.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Creational\SimpleFactory;
+
+class Car
+{
+    /**
+     * @var string
+     */
+    private $type;
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+}
